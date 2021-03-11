ffa09790a11f558f7f7ae4c9ca076acc0a6e82f1
stripyterror <larshuiting@gmail.com>
2021-03-11 16:52:16 +0100

    Can assign a position to the square by pressing P

---
+++ common/saveload.h
@@ -0,0 +1,7 @@
+#ifndef SAVELOAD_H
+#define SAVELOAD_H
+
+#include <fstream>
+
+
+#endif
--- demo/demo.cpp
+++ demo/demo.cpp
@@ -16,17 +16,13 @@ int main( void )
 
 	Scene* scene = new Scene();
 
-	Sprite* kingkong = new Sprite("assets/kingkong.tga");
-	kingkong->position = glm::vec3(400.0f, 300.0f, 0.0f);
+	double xpos, ypos;
+
 	Sprite* pencils = new Sprite("assets/pencils.tga");
 	pencils->position = glm::vec3(900.0f, 300.0f, 0.0f);
-	Sprite* rgba = new Sprite("assets/rgba.tga");
-	rgba->position = glm::vec3(WIDTH/2, HEIGHT/2, 0.0f);
-	rgba->scale = glm::vec3(3.0f, 3.0f, 1.0f);
 
-	scene->addSprite(kingkong);
+
 	scene->addSprite(pencils);
-	scene->addSprite(rgba);
 
 	do {
 		// get deltaTime and update camera
@@ -35,7 +31,13 @@ int main( void )
 
 		// Update the scene
 		scene->update(dt); // TODO make subclass for Scene (and make Scene abstract?)
-		rgba->rotation += dt; // for now, do it here
+		glfwGetCursorPos(renderer.window(), &xpos, &ypos);
+		if (glfwGetKey(renderer.window(), GLFW_KEY_P))
+		{
+			pencils->position.x = xpos;
+			pencils->position.y = ypos;
+		}
+
 
 		// Render the scene
 		renderer.renderScene(scene);
